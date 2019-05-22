40924871ff005f2f773b7603b02f4bd044a3a93c
Nuno Diogo <nunolsdiogo@hotmail.com>
2019-05-22 15:38:32 +0100

    started making layout of the app

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
 <?php
     include('functions.php');
+    $loggedIn = false;
+    if(isset($_SESSION['username']))
+    {
+        $username = $_SESSION['username'];
+        if($username!="")
+            $loggedIn = true;
+    }
 ?>
 <html>
     <head>
+        <script src="http://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
         <?php HeaderImports(); ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8"> 
     </head>
-    <body>
+    <body onload="startTime()">
+        <header class="page-header header container-fluid">
+            <nav class="navbar navbar-expand-md">
+                <a class="navbar-brand" href="index.php">Logo</a>
+                <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="main-navigation">
+                    <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <?php
+                            if($loggedIn)
+                            {
+                        ?>
+                                <p class="nav-link"><?php echo $_SESSION['username']; ?></p>
+                                <a class="nav-link" href="logout.php">Logout</a>
+                        <?php
+                            }
+                            else
+                            {
+                        ?>
+                                <a class="nav-link" href="login.php">Login/Register</a>
+                        <?php
+                            }
+                        ?>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div class="page-content content container-fluid">
+            <h1 class="page-title title">Schedule Manager</h1><br>
+            <span id="dateTime"></span>
+        </div>
+        <footer class="page-footer footer container-fluid">
+
+        </footer>
     </body>
 </html>
---
+++ js/global.js
@@ -0,0 +1,45 @@
+//Global JS Functions
+    function startTime()
+    {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        var dd = today.getDate();
+
+        var mm = today.getMonth()+1; 
+        var yyyy = today.getFullYear();
+        if(dd<10) 
+        {
+            dd='0'+dd;
+        } 
+
+        if(mm<10) 
+        {
+            mm='0'+mm;
+        } 
+
+        var dateString = dd + "/" + mm + "/" + yyyy;
+
+        m = checkTime(m);
+        s = checkTime(s);
+
+        var weekday = new Array(7);
+        weekday[0] =  "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        var dayOfWeek = weekday[today.getDay()];
+
+        document.getElementById('dateTime').innerHTML = dayOfWeek + " " + dateString + " " + h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) 
+    {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
