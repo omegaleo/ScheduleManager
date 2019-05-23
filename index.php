6d72879c768b7a9a06783e8f83ee29d20cf54a08
Nuno Diogo <nunolsdiogo@hotmail.com>
2019-05-23 19:42:44 +0100

    Finished first version of the layout

--- index.php
+++ index.php
@@ -16,41 +16,43 @@
         <meta charset="utf-8"> 
     </head>
     <body onload="startTime()">
-        <header class="page-header header container-fluid">
-            <nav class="navbar navbar-expand-md">
-                <a class="navbar-brand" href="index.php">Logo</a>
-                <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="main-navigation">
-                    <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <?php
-                            if($loggedIn)
-                            {
-                        ?>
-                                <p class="nav-link"><?php echo $_SESSION['username']; ?></p>
-                                <a class="nav-link" href="logout.php">Logout</a>
-                        <?php
-                            }
-                            else
-                            {
-                        ?>
-                                <a class="nav-link" href="login.php">Login/Register</a>
-                        <?php
-                            }
-                        ?>
-                    </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <div class="page-content content container-fluid">
-            <h1 class="page-title title">Schedule Manager</h1><br>
-            <span id="dateTime"></span>
+        <div class="box">
+            <header class="page-header header container-fluid">
+                <nav class="navbar navbar-expand-md">
+                    <a class="navbar-brand" href="index.php">Logo</a>
+                    <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="main-navigation">
+                        <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <?php
+                                if($loggedIn)
+                                {
+                            ?>
+                                    <p class="nav-link"><?php echo $_SESSION['username']; ?></p>
+                                    <a class="nav-link" href="logout.php">Logout</a>
+                            <?php
+                                }
+                                else
+                                {
+                            ?>
+                                    <a class="nav-link" href="login.php">Login/Register</a>
+                            <?php
+                                }
+                            ?>
+                        </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <div class="page-content content container-fluid">
+                <h1 class="page-title title">Schedule Manager</h1><br>
+                <span id="dateTime"></span>
+            </div>
+            <footer class="page-footer footer container-fluid fixed-bottom">
+                <span class="madeBy">Made by <a href="https://omegaleo.pt" targt="_blank"><img src="img/Logo.png"></a></span>
+            </footer>
         </div>
-        <footer class="page-footer footer container-fluid">
-
-        </footer>
     </body>
 </html>
