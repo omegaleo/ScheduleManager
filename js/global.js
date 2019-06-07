0eabe38682d94b8b55932a93f783a1114c8f88f4
Nuno Diogo <nunolsdiogo@hotmail.com>
2019-06-07 22:08:31 +0100

    More fixes to cookie handling

--- js/global.js
+++ js/global.js
@@ -213,12 +213,15 @@ var notifiedTasks = [];
         if(cookies!=undefined && cookies!=null && cookies.length>0)
         {
             for (var i = 0; i < cookies.length; i++) {
-                var name = removeSpaces(cookies[i].split('=')[0].toLowerCase());
-                var value = removeSpaces(cookies[i].split('=')[1].toLowerCase());
-                if (name == input) {
-                  return value;
-                } else if (value == input) {
-                  return name;
+                if(cookies[i]!="" && cookies[i]!=null && cookies[i]!=undefined)
+                {
+                    var name = removeSpaces(cookies[i].split('=')[0].toLowerCase());
+                    var value = removeSpaces(cookies[i].split('=')[1].toLowerCase());
+                    if (name == input) {
+                      return value;
+                    } else if (value == input) {
+                      return name;
+                    }
                 }
               }
               return "";
