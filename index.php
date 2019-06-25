0255cbe069746ef9791df57fa40c2fc13ea950f4
Nuno Diogo <nunodiogo@omegaleo.pt>
2019-06-25 15:40:45 +0100

    Attempt at fixing the navbar problems in mobile

--- index.php
+++ index.php
@@ -21,11 +21,11 @@
             <header class="page-header header container-fluid">
                 <nav class="navbar navbar-expand-md">
                     <a class="navbar-brand" href="index.php">Schedule Manager</a>
-                    <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation" aria-controls="main-navigation" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="main-navigation">
-                        <ul class="navbar-nav">
+                        <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
                             <!--<button onclick="DarkModeToggle()">Dark Mode</button>-->
                             <div id="darkModeSwitch" class="toggle-btn" onclick="DarkModeToggle()">
